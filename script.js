a4ce3a9c5a6f606677d21f298d8e66cecab8e2c1
Krish-Makadiya <krishmakadiya2005@gmail.com>
2024-06-18 12:35:31 +0530

    Final Commit with responsiveness

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 let button = document.querySelector('.share');
 let modalSection = document.querySelector('.modal');
 let closeBtn = document.querySelector('#closeBtn');
+let overlay = document.querySelector('.overlay');
 
 button.addEventListener('click', ()=>{
     modalSection.classList.add("active");
+    overlay.classList.add("overlay-active");
 })
 
 closeBtn.addEventListener('click', ()=>{
     modalSection.classList.remove('active');
+    overlay.classList.remove("overlay-active");
+})
+
+overlay.addEventListener('click', ()=>{
+    modalSection.classList.remove('active');
+    overlay.classList.remove("overlay-active");
 })
